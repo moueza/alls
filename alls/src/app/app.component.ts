52bb39a0924eaf566013474d5c24ff125c70e928
moueza <>
2020-07-31 10:03:03 +0200

    server OK : v04 clickable value ; deploy

--- alls/src/app/app.component.ts
+++ alls/src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+      templateUrl: './app.component.html',
+      styleUrls: ['./app.component.scss']
+      })
 export class AppComponent {
   title = 'alls';
+  value='';
+
+  updateValue(){
+    this.value='value of string';
+  }
+
+
+  nothing(){
+   
+  }
 }
