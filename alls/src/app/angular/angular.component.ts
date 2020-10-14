0de5f61d5b9a73bc43304d4c360d8650f21aa9d4
moueza <>
2020-10-14 14:38:45 +0200

    See #28 Logging (session). See #1 gen : local compiles OK

--- alls/src/app/angular/angular.component.ts
+++ alls/src/app/angular/angular.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AngularComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('\nAngular');
+
+  }
 
   ngOnInit(): void {
   }
--- alls/src/app/cobol/cobol.component.ts
+++ alls/src/app/cobol/cobol.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CobolComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('\ncobol');
+  }
 
   ngOnInit(): void {
   }
