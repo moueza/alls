4a1bdd8f1dc071864ccc01b4989df07e5d77d50f
moueza <>
2020-10-14 10:44:28 +0200

    v038 COBOL Angular functions moi. See #1 gen / OK + local compiles OK

--- alls/src/app/cobol/cobol.component.ts
+++ alls/src/app/cobol/cobol.component.ts
@@ -12,4 +12,8 @@ export class CobolComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  URLing(){}
+  minutesToSeconds(){}
+  timeliner(url="https://www.youtube.com/watch?v=ycHXzbAmY94&feature=emb_logo&ab_channel=IBMDeveloper",textToBeTreated="14:57 characteristics NJ UI 17:55 editor explorer 21:09 program content 24:15 Z editor 27:31 ZOWER Explorer 29:46 California government apprenticeship programs developers shortage 46:12 Mainframes 47:40 courses 51:00 volunteers 53:20 newsletters subscribe 54:10 social 56:29 Student Hub"){}
 }
