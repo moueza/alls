8888a8b13f804ca2e71cb09af21d2972b9a265a7
moueza <>
2020-10-20 14:41:18 +0200

    see #1 gen : compile OK

--- alls/src/app/javascript-anim/javascript-anim.component.ts
+++ alls/src/app/javascript-anim/javascript-anim.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+//import * as JavAnim from './../pages/FriendCard';
+import * as JavAnim from  './javascript-anim';
+
 @Component({
   selector: 'app-javascript-anim',
   templateUrl: './javascript-anim.component.html',
-  styleUrls: ['./javascript-anim.component.scss']
+  styleUrls: ['./javascript-anim.component.scss'],
 })
 export class JavascriptAnimComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    //window.canvas
   }
 
+  ngOnInit(): void {}
 
-javascriptAnim(){
+  //javascriptAnim(){ to  file javascript-anim
 }
-
-}
-
---
+++ alls/src/app/javascript-anim/javascript-anim.js
@@ -0,0 +1,55 @@
+//https://stackoverflow.com/questions/41219542/how-to-import-js-modules-into-typescript-file
+var JavAnim = function () {
+  p1, p2, p3, p4;
+
+  dirPerUnitVect1, dirPerUnitVect2, dirPerUnitVect3, dirPerUnitVect4;
+
+  function JavascriptAnim() {
+    htG.x = 0;
+    htG.x = 0;
+    htG.x = 0;
+    htG.x = 0;
+
+    pnt1.x;
+    pnt1.y;
+
+    pnt2.x;
+    pnt2.y;
+
+    pnt3.x;
+    pnt3.y;
+
+    pnt4.x;
+    pnt4.y;
+  }
+
+  //https://www.w3schools.com/js/js_object_prototypes.asp
+  //Prototype
+  function Person(first, last, age, eyecolor) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eyecolor;
+  }
+
+  var myFather = new Person("John", "Doe", 50, "blue");
+  var myMother = new Person("Sally", "Rally", 48, "green");
+
+  function PointMoi(pp1, pp2, pp3, pp4) {
+    this.p1 = pp1;
+    this.p2 = pp2;
+    this.p3 = pp3;
+    this.p4 = pp4;
+  }
+
+  function vectMoi(vect1,vect2,vect3,vect4){
+   this.dirPerUnitVect1=Vect
+  }
+
+  //'back...
+  function unitUpdate(timeRelativeUnit) {
+    this.p1
+  }
+};
+
+module.exports = JavAnim;
